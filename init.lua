c0d7e129a3efaedc4a3f63bc0ab83cc472a3da98
Elliot Dickerson <elliot@toille.uk>
2023-08-11 19:38:28 +0100

    Initial setup

---
+++ init.lua
@@ -0,0 +1,14 @@
+return {
+  plugins = {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.pack.docker" },
+    { import = "astrocommunity.pack.html-css" },
+    { import = "astrocommunity.pack.json" },
+    { import = "astrocommunity.pack.lua" },
+    { import = "astrocommunity.pack.php" },
+    { import = "astrocommunity.pack.tailwindcss" },
+    { import = "astrocommunity.pack.toml" },
+    { import = "astrocommunity.pack.typescript" },
+    { import = "astrocommunity.pack.yaml" },
+  },
+}
